defb9a368dd8e317e26f8e662e59c4ce2a929849
Turcu11 <92573479+Turcu11@users.noreply.github.com>
2022-06-23 11:01:02 +0300

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,18 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		AltExemplu ae = new Exemplu();
+		/*
+		 * ideea ii ca Exemplu il mosteneste pe AltExemplu de aia ni se permite
+		 * instantierea lui astfel, si aici se vede din nou legarea dinamica
+		 */
+
+		/*
+		 * o a doua optiune ar fi legata tot de mostenire dar sa fie pe mai multe nivele
+		 * cum ar fi sa avem AltExemplu care sa fie mostenit de inca 3 clase pana la
+		 * Exemolu si in cazul acela ar funcitona
+		 */
+	}
+
+}
